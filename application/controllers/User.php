5a01afa3365319612fc305a6c6526ec0fea51320
BagusSatria123 <satriabagusputra123@gmail.com>
2020-09-26 22:49:52 +0700

    Form Register

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -9,7 +9,7 @@ class User extends CI_Controller {
     public function register(){
         $data['title'] = "Register";
 
-        $this->load->view('template/header',$data);
+    $this->load->view('template/header',$data);
 		$this->load->view('register',$data);
 		$this->load->view('template/footer',$data);
     }
